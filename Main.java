0cf77159701822271ac1811e08497b4910bb67cd
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2023-12-18 13:30:03 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.lang.*;
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Calculator 
+{
+    public static void main(String[]args)
+    {
+        double num1, num2;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your two values");
+        
+    }
+}
